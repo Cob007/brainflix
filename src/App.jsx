df34ef78e7acc6f89d255c0c549f0879b52a12da
Cob007 <godwill2beracah@outlook.com>
2024-03-05 12:43:14 -0500

    Fetching json data locally added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import './App.scss'
-import Header from './components/Header/Header'
+import "./App.scss";
+import Header from "./components/Header/Header";
+import videos from "./data/videos.json";
+import videoDetails from "./data/video-details.json";
 
 const App = () => {
+  //console.log("videos :", videos);
+  console.log("videos :", videoDetails);
+  console.log("videos :", videoDetails[0].comments);
+
 
   return (
     <>
-    <Header />
+      <Header />
       
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
