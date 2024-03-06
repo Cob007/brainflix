5c4a7bf0154e7b92ffe8cd50c00ac35bafc0897f
Cob007 <godwill2beracah@outlook.com>
2024-03-06 12:59:27 -0500

    comment cards update  added

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,19 +1,26 @@
-import PostComment from '../PostComment/PostComment';
-import './Comment.scss'
+import CommentCard from "../CommentCard/CommentCard";
+import PostComment from "../PostComment/PostComment";
+import "./Comment.scss";
 
 const Comment = (props) => {
+  const { commentList } = props;
 
-    const { commentList } = props;
-
-    console.log("Hi")
-
-    return (
-    <section className='comment'>
-        <h3 className='comment__count'>{commentList.length} Comments</h3>
-        <PostComment />
-    
+  return (
+    <section className="comment">
+      <h3 className="comment__count">{commentList.length} Comments</h3>
+      <PostComment />
+      <ul className="comment__comment-list">
+        {commentList.map((commentObj, index) => (
+          <li key={index}>
+            <>         
+            <div className="comment__divider"/> 
+              <CommentCard comment={commentObj} />
+            </>
+          </li>
+        ))}
+      </ul>
     </section>
-    )
-}
+  );
+};
 
 export default Comment;
