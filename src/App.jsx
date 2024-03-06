62ea5232816cda9e0b9e91ec9df71fd2a34ee382
Cob007 <godwill2beracah@outlook.com>
2024-03-06 16:42:38 -0500

    Next Video layout update added

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,28 @@ import "./App.scss";
 import Header from "./components/Header/Header";
 import videos from "./data/videos.json";
 import videoDetails from "./data/video-details.json";
-import NextVideo from "./components/NextVideo/NextVideo";
 import PlayNow from "./components/PlayNow/PlayNow";
-import Comment from "./components/Comment/Comment";
-import VideoDescription from "./components/VideoDescription/VideoDescription";
+import { useState } from "react";
+import CommentDespNextVideo from "./components/CommentDespNextVideo/CommentDespNextVideo";
 
 const App = () => {
-  console.log("videos :", videos);
-  console.log("videos :", videoDetails);
-  console.log("videos :", videoDetails[0].comments);
+  const [selectedVideo, setSelectedVideo] = useState({});
+  //currentVideo
+
+  const handleClick = (video) => {
+
+
+  };
+
   return (
     <div className="container">
       <Header />
-      <PlayNow videoClicked={videos[0]}/>
-      <VideoDescription clickedVideo={videoDetails[0]}/>
-      <Comment commentList={videoDetails[0].comments}/>
-      <NextVideo videoList={videos}/>
+      <PlayNow videoClicked={videos[0]} />
+      <CommentDespNextVideo
+        videos={videos}
+        videoDetails={videoDetails}
+        handleClick={handleClick}
+      />
     </div>
   );
 };
